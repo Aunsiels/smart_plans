eff0e1b4f1d34052baa6247e3f751781b5079203
Julien Romero <romerojulien34@gmail.com>
2018-02-20 15:27:56 +0100

    utils for stack and unstack

--- prototype/fake_tree_function.py
+++ prototype/fake_tree_function.py
@@ -1,11 +1,11 @@
-from consommation_rule import ConsommationRule
-from production_rule import ProductionRule
 from duplication_rule import DuplicationRule
 from function import Function
+from utils import stack, unstack
 
 
 class FakeTreeFunction(object):
-    """FakeTreeFunction Represents a faketree function"""
+    """FakeTreeFunction Represents a faketree function -  For implementation
+    purposes"""
 
     def __init__(self, head, others, name="tf"):
         """__init__
@@ -124,81 +124,3 @@ class FakeTreeFunction(object):
             counter = temp[1]
             rules = rules + temp[0]
         return (rules, counter)
-
-
-def unstack(u_relations, all_relations, counter, start, end):
-    """unstack
-    Unstack the given relation from the stack by using reduced forms
-    :param u_relations: The relations to unstack
-    :param all_relations: All the relations used
-    :param counter: Counter to avoid collision
-    :param start: the starting non-terminal
-    :param end: the ending non-terminal
-    """
-    rules = []
-    if len(u_relations) != 0:
-        rules.append(DuplicationRule(start,
-                                     "T",
-                                     "C" + str(counter)))
-    else:
-        rules.append(DuplicationRule(start,
-                                     "T",
-                                     end))
-    temp_counter = counter
-    if len(u_relations) > 1:
-        rules.append(ConsommationRule(u_relations[0],
-                                      "C" + str(counter),
-                                      "C" + str(counter + 1)))
-        counter += 1
-    elif len(u_relations) == 1:
-        rules.append(ConsommationRule(u_relations[0],
-                                      "C" + str(counter),
-                                      end))
-    for x in range(1, len(u_relations) - 1):
-        rules.append(ConsommationRule(u_relations[x],
-                                      "C" + str(counter),
-                                      "C" + str(counter + 1)))
-        counter += 1
-    if len(u_relations) > 1:
-        rules.append(ConsommationRule(u_relations[-1],
-                                      "C" + str(counter),
-                                      end))
-    for x in range(len(all_relations)):
-        temp_counter += 1
-    counter = max(temp_counter, counter)
-    counter += 1
-    return (rules, counter)
-
-
-def stack(s_relations, counter, start, end):
-    """stack
-    Stack the given relations on the stack using reduced rules.
-    :param s_relations: The relations to stack
-    :param counter: The counter to avoid collisions
-    :param start: The starting non-terminal
-    :param end: The ending non-terminal
-    """
-    rules = []
-    if len(s_relations) == 0:
-        rules.append(DuplicationRule(start,
-                                     "T",
-                                     end))
-    elif len(s_relations) == 1:
-        rules.append(ProductionRule(start,
-                                    end,
-                                    s_relations[0]))
-    else:
-        rules.append(ProductionRule(start,
-                                    "A" + str(counter),
-                                    s_relations[0]))
-    for x in range(1, len(s_relations) - 1):
-        rules.append(ProductionRule("A" + str(counter),
-                                    "A" + str(counter + 1),
-                                    s_relations[x]))
-        counter += 1
-    if len(s_relations) > 1:
-        rules.append(ProductionRule("A" + str(counter),
-                                    end,
-                                    s_relations[-1]))
-    counter += 1
-    return (rules, counter)
--- prototype/function.py
+++ prototype/function.py
@@ -1,8 +1,7 @@
 import re
-from consommation_rule import ConsommationRule
-from production_rule import ProductionRule
 from duplication_rule import DuplicationRule
 import sys
+from utils import stack, unstack
 
 
 class Function (object):
@@ -297,84 +296,6 @@ class Function (object):
         return self.relations[-1][0] + 'm' * self.relations[-1][1]
 
 
-def unstack(u_relations, all_relations, counter, start, end):
-    """unstack
-    Unstack the given relation from the stack by using reduced forms
-    :param u_relations: The relations to unstack
-    :param all_relations: All the relations used
-    :param counter: Counter to avoid collision
-    :param start: the starting non-terminal
-    :param end: the ending non-terminal
-    """
-    rules = []
-    if len(u_relations) != 0:
-        rules.append(DuplicationRule(start,
-                                     "T",
-                                     "C" + str(counter)))
-    else:
-        rules.append(DuplicationRule(start,
-                                     "T",
-                                     end))
-    temp_counter = counter
-    if len(u_relations) > 1:
-        rules.append(ConsommationRule(u_relations[0],
-                                      "C" + str(counter),
-                                      "C" + str(counter + 1)))
-        counter += 1
-    elif len(u_relations) == 1:
-        rules.append(ConsommationRule(u_relations[0],
-                                      "C" + str(counter),
-                                      end))
-    for x in range(1, len(u_relations) - 1):
-        rules.append(ConsommationRule(u_relations[x],
-                                      "C" + str(counter),
-                                      "C" + str(counter + 1)))
-        counter += 1
-    if len(u_relations) > 1:
-        rules.append(ConsommationRule(u_relations[-1],
-                                      "C" + str(counter),
-                                      end))
-    for x in range(len(all_relations)):
-        temp_counter += 1
-    counter = max(temp_counter, counter)
-    counter += 1
-    return (rules, counter)
-
-
-def stack(s_relations, counter, start, end):
-    """stack
-    Stack the given relations on the stack using reduced rules.
-    :param s_relations: The relations to stack
-    :param counter: The counter to avoid collisions
-    :param start: The starting non-terminal
-    :param end: The ending non-terminal
-    """
-    rules = []
-    if len(s_relations) == 0:
-        rules.append(DuplicationRule(start,
-                                     "T",
-                                     end))
-    elif len(s_relations) == 1:
-        rules.append(ProductionRule(start,
-                                    end,
-                                    s_relations[0]))
-    else:
-        rules.append(ProductionRule(start,
-                                    "A" + str(counter),
-                                    s_relations[0]))
-    for x in range(1, len(s_relations) - 1):
-        rules.append(ProductionRule("A" + str(counter),
-                                    "A" + str(counter + 1),
-                                    s_relations[x]))
-        counter += 1
-    if len(s_relations) > 1:
-        rules.append(ProductionRule("A" + str(counter),
-                                    end,
-                                    s_relations[-1]))
-    counter += 1
-    return (rules, counter)
-
-
 def test():
     """test Performs some basic tests"""
     relations = ['a', 'b--', 'c-']
@@ -397,6 +318,3 @@ def test():
         print(rule)
     print(right_red[1])
     print("All is good")
-
-
-# test()
